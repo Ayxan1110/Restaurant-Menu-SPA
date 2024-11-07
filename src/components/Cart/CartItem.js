294d53f54ab64c9aa5f2ce49932f5d08df11614c
Ayxan1110 <106319113+Ayxan1110@users.noreply.github.com>
2024-11-07 15:19:34 +0400

    Update CartItem.js

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -6,39 +6,41 @@ function CartItem({ item, onRemove }){
     const [modal, setModal] = useState(false)
 
     return (
-        <div className="cart-item flex justify-between items-center border-b border-b-2 border-black py-2">
-            <div>
-                <img className="max-h-24 w-auto" src={`${item.img}`} alt={item.title} />
-            </div>
-            <div className='cart-item-name'>
-                <h2 className="text-lg font-semibold">{item.title}</h2>
-                <p className="text-gray-700 text-2xl">${item.price.toFixed(2)}</p>
-            </div>
-            <div className='cart-item-name'>
-                <ul className="text-md grid grid-rows-3 grid-flow-col">
-                    {item.selectedToppings
-                        ?
-                        item.selectedToppings.map((item, index) => (
-                            <li key={index}>{item}</li>
-                        ))
-                        :
-                        item.toppings.map((item, index) => (
-                            <li key={index}>{item}</li>
-                        ))
-                    }
-                </ul>
+        <div className="flex flex-col items-center justify-between py-2 border-b-2 border-black md:flex-row">
+            <div className='flex gap-1'>
+                <div>
+                    <img className="w-auto max-h-24" src={`${item.img}`} alt={item.title} />
+                </div>
+                <div className='cart-item-name'>
+                    <h2 className="text-lg font-semibold">{item.title}</h2>
+                    <p className="text-2xl text-gray-700">${item.price.toFixed(2)}</p>
+                </div>
+                <div className='cart-item-name'>
+                    <ul className="grid grid-flow-col grid-rows-3 text-md">
+                        {item.selectedToppings
+                            ?
+                            item.selectedToppings.map((item, index) => (
+                                <li key={index}>{item}</li>
+                            ))
+                            :
+                            item.toppings.map((item, index) => (
+                                <li key={index}>{item}</li>
+                            ))
+                        }
+                    </ul>
+                </div>
             </div>
             <div class="cart-buttons">
                 <div>
                 <button
                     onClick={onRemove}
-                    className="bg-red-500 text-white px-4 py-2 text-xl rounded hover:bg-red-600 transition duration-300"
+                    className="px-4 py-2 text-xl text-white transition duration-300 bg-red-500 rounded hover:bg-red-600"
                 >
                     Remove
                 </button>
                 <button
                     onClick={() => setModal(true)}
-                    className="ml-5 bg-yellow-500 text-white px-4 py-2 text-xl rounded hover:bg-yellow-600 transition duration-300"
+                    className="px-4 py-2 ml-5 text-xl text-white transition duration-300 bg-yellow-500 rounded hover:bg-yellow-600"
                 >
                     Modify
                 </button>
